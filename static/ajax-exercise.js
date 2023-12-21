0e472f0517c4c7428fd51203799cd073aaff4935
Edilene Sampaio <edilenesampaio7@gmail.com>
2023-12-20 19:25:45 -0600

    Final commit

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -6,7 +6,6 @@ function showFortune(evt) {
   fetch('/fortune')
     .then((response) => response.text())
     .then(replaceFortune);
-
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
@@ -19,21 +18,55 @@ function replaceFortune(results) {
 
 function showWeather(evt) {
   evt.preventDefault();
-
-  const url = '/weather.json';
   const zipcode = document.querySelector('#zipcode-field').value;
-
-  // TODO: request weather with that URL and show the forecast in #weather-info
+  fetch(`/weather.json?zipcode=${zipcode}`)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      document.querySelector('#weather-info').innerHTML = jsonData.forecast;
+    });
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
+
+
 // PART 3: ORDER MELONS
+
+function updateMelons(results) {
+  if (results.code === 'OK') {
+    document.querySelector('#order-status').classList.remove('order-error');
+    document.querySelector('#order-status').innerHTML = `<p>${results.msg}</p>`;
+  } else {
+    document.querySelector('#order-status').classList.add('order-error');
+    document.querySelector('#order-status').innerHTML = `<p><b>${results.msg}</b></p>`;
+  }
+}
 
 function orderMelons(evt) {
   evt.preventDefault();
+  const formInputs = {
+    melon_type: document.querySelector('#melon-type-field').value,
+    qty: document.querySelector('#qty-field').value,
+  };
+  fetch('/order-melons.json', {
+    method: 'POST',
+    body: JSON.stringify(formInputs),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(updateMelons);
+}
 
+document.querySelector('#order-form').addEventListener('submit', orderMelons);
+    //   evt.preventDefault();
+    
+    //   const formInputs = {
+    //     type: document.querySelector('#melon-type-field').value,
+    //     amount: document.querySelector('#amount-field').value,
+    //   };
+    
+      
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
-document.querySelector('#order-form').addEventListener('submit', orderMelons);
